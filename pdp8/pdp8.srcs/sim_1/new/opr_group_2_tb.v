08ea0d71b970836b32b4fd8817bf624f4c1348ed
Sander Huijsen <ahuijsen@pm.me>
2020-04-15 20:17:07 +0800

    Completely refactored the test for OPR Group 2 using a task which does the heavy lifting.

--- pdp8/pdp8.srcs/sim_1/new/opr_group_2_tb.v
+++ pdp8/pdp8.srcs/sim_1/new/opr_group_2_tb.v
@@ -22,228 +22,123 @@
 
 
 module opr_group_2_tb;
-    integer i = 0;
 
-    reg [0:11] ac_reg, i_reg;
-    reg l_reg;
-    wire skip_flag;
+reg [0:11] ac_reg, i_reg;
+reg l_reg;
+wire skip_flag;
 
-    integer skip_flag_result = 0; 
+localparam period = 20;
 
-    localparam period = 20;
+`include "do_skip.vh"
 
-    `include "do_skip.vh"
+assign skip_flag = do_skip(l_reg, ac_reg, i_reg);
 
-    assign skip_flag = do_skip(l_reg, ac_reg, i_reg);
+integer skip_flag_result, i;
 
-    initial
-    begin
-        $display("---=== OR  GROUP ===---");
 
-        // OR group - None of the predicates are true
-        $display("test 1");
-        ac_reg = 1;
-        l_reg = 0;
-        i_reg = 12'b11110xxx0000;
-        for (i=0; i<8; i=i+1) begin
-            i_reg = {i_reg[0:4], i, i_reg[8:11]};
-            #period;
-            skip_flag_result = skip_flag_result << 1 | skip_flag;
-        end
-        if (skip_flag_result)
-            $display("test failed: skip_flag expected to be 0 but got %08b", skip_flag_result);
-        else
-            $display("ok.");
+task simulate;
 
-        skip_flag_result = 0;
+input integer num;
+input [0:7] expected_result;
 
-        // OR group - Only SNL is true
-        $display("test 2");
-        ac_reg = 1;
-        l_reg = 1;
-        i_reg = 12'b11110xxx0000;
-        for (i=0; i<8; i=i+1) begin
-            i_reg = {i_reg[0:4], i, i_reg[8:11]};
-            #period;
-            skip_flag_result = skip_flag_result | skip_flag << i;
-        end
-        if (skip_flag_result != 8'b10101010)
-            $display("test failed: expected 10101010 but got %08b", skip_flag_result);
-        else
-            $display("ok.");
-
-        skip_flag_result = 0;
-
-        // OR group - Only SZA is true
-        $display("test 3");
-        ac_reg = 0;
-        l_reg = 0;
-        i_reg = 12'b11110xxx0000;
-        for (i=0; i<8; i=i+1) begin
-            i_reg = {i_reg[0:4], i, i_reg[8:11]};
-            #period;
-            skip_flag_result = skip_flag_result | skip_flag << i;
-        end
-        if (skip_flag_result != 8'b11001100)
-            $display("test failed: expected 11001100 but got %08b", skip_flag_result);
-        else
-            $display("ok.");
-
-        skip_flag_result = 0;
-
-        // OR group - Only SMA is true
-        $display("test 4");
-        ac_reg = -1;
-        l_reg = 0;
-        i_reg = 12'b11110xxx0000;
-        for (i=0; i<8; i=i+1) begin
-            i_reg = {i_reg[0:4], i, i_reg[8:11]};
-            #period;
-            skip_flag_result = skip_flag_result | skip_flag << i;
-        end
-        if (skip_flag_result != 8'b11110000)
-            $display("test failed: expected 11110000 but got %08b", skip_flag_result);
-        else
-            $display("ok.");
-
-        skip_flag_result = 0;
-
-        // OR group - SMA and SNL are true
-        $display("test 5");
-        ac_reg = -1;
-        l_reg = 1;
-        i_reg = 12'b11110xxx0000;
-        for (i=0; i<8; i=i+1) begin
-            i_reg = {i_reg[0:4], i, i_reg[8:11]};
-            #period;
-            skip_flag_result = skip_flag_result | skip_flag << i;
-        end
-        if (skip_flag_result != 8'b11111010)
-            $display("test failed: expected 11111010 but got %08b", skip_flag_result);
-        else
-            $display("ok.");
-
-        skip_flag_result = 0;
-
-        // OR group - SZA and SNL are true
-        $display("test 6");
-        ac_reg = 0;
-        l_reg = 1;
-        i_reg = 12'b11110xxx0000;
-        for (i=0; i<8; i=i+1) begin
-            i_reg = {i_reg[0:4], i, i_reg[8:11]};
-            #period;
-            skip_flag_result = skip_flag_result | skip_flag << i;
-        end
-        if (skip_flag_result != 8'b11101110)
-            $display("test failed: expected 11101110 but got %08b", skip_flag_result);
-        else
-            $display("ok.");
-
-        $display("---=== AND GROUP ===---");
-
-        skip_flag_result = 0;
-
-        // AND group - All of the predicates are true
-        $display("test 1");
-        ac_reg = 1;
-        l_reg = 0;
-        i_reg = 12'b11110xxx1000;
-        for (i=0; i<8; i=i+1) begin
-            i_reg = {i_reg[0:4], i, i_reg[8:11]};
-            #period;
-            skip_flag_result = skip_flag_result | skip_flag << i;
-        end
-        if (skip_flag_result != 8'b11111111)
-            $display("test failed: expected 11111111 but got %08b", skip_flag_result);
-        else
-            $display("ok.");
-
-        skip_flag_result = 0;
-
-        // AND group - SZL is not true
-        $display("test 2");
-        ac_reg = 1;
-        l_reg = 1;
-        i_reg = 12'b11110xxx1000;
-        for (i=0; i<8; i=i+1) begin
-            i_reg = {i_reg[0:4], i, i_reg[8:11]};
-            #period;
-            skip_flag_result = skip_flag_result | skip_flag << i;
-        end
-        if (skip_flag_result != 8'b01010101)
-            $display("test failed: expected 01010101 but got %08b", skip_flag_result);
-        else
-            $display("ok.");
-
-        skip_flag_result = 0;
-
-        // AND group - SNA is not true
-        $display("test 3");
-        ac_reg = 0;
-        l_reg = 0;
-        i_reg = 12'b11110xxx1000;
-        for (i=0; i<8; i=i+1) begin
-            i_reg = {i_reg[0:4], i, i_reg[8:11]};
-            #period;
-            skip_flag_result = skip_flag_result | skip_flag << i;
-        end
-        if (skip_flag_result != 8'b00110011)
-            $display("test failed: expected 00110011 but got %08b", skip_flag_result);
-        else
-            $display("ok.");
-
-        skip_flag_result = 0;
-
-        // AND group - SPA is not true
-        $display("test 4");
-        ac_reg = -1;
-        l_reg = 0;
-        i_reg = 12'b11110xxx1000;
-        for (i=0; i<8; i=i+1) begin
-            i_reg = {i_reg[0:4], i, i_reg[8:11]};
-            #period;
-            skip_flag_result = skip_flag_result | skip_flag << i;
-        end
-        if (skip_flag_result != 8'b00001111)
-            $display("test failed: expected 00001111 but got %08b", skip_flag_result);
-        else
-            $display("ok.");
-
-        skip_flag_result = 0;
-
-        // AND group - SPA and SZL are not true
-        $display("test 5");
-        ac_reg = -1;
-        l_reg = 1;
-        i_reg = 12'b11110xxx1000;
-        for (i=0; i<8; i=i+1) begin
-            i_reg = {i_reg[0:4], i, i_reg[8:11]};
-            #period;
-            skip_flag_result = skip_flag_result | skip_flag << i;
-        end
-        if (skip_flag_result != 8'b00000101)
-            $display("test failed: expected 00000101 but got %08b", skip_flag_result);
-        else
-            $display("ok.");
-
-        skip_flag_result = 0;
-
-        // AND group - SNA and SZL are not true
-        $display("test 6");
-        ac_reg = 0;
-        l_reg = 1;
-        i_reg = 12'b11110xxx1000;
-        for (i=0; i<8; i=i+1) begin
-            i_reg = {i_reg[0:4], i, i_reg[8:11]};
-            #period;
-            skip_flag_result = skip_flag_result | skip_flag << i;
-        end
-        if (skip_flag_result != 8'b00010001)
-            $display("test failed: expected 00010001 but got %08b", skip_flag_result);
-        else
-            $display("ok.");
-
+begin
+    $display("test %2d", num);
+    skip_flag_result = 0;
+    for (i=0; i<8; i=i+1) begin
+        i_reg = {i_reg[0:4], i, i_reg[8:11]};
+        #period;
+        skip_flag_result = skip_flag_result | skip_flag << i;
     end
+    if (skip_flag_result != expected_result)
+    begin
+        $display("test failed: skip_flag expected to be %08b but got %08b.", expected_result, skip_flag_result);
+        $display("stop.");
+        $finish;
+    end
+    else
+        $display("ok.");
+end
+
+endtask
+
+initial
+begin
+    $display("---=== OR  GROUP ===---");
+
+    // OR group - None of the predicates are true
+    ac_reg = 1;
+    l_reg = 0;
+    i_reg = 12'b11110xxx0000;
+    simulate(1, 8'b0);
+
+    // OR group - Only SNL is true
+    ac_reg = 1;
+    l_reg = 1;
+    i_reg = 12'b11110xxx0000;
+    simulate(2, 8'b10101010);
+
+    // OR group - Only SZA is true
+    ac_reg = 0;
+    l_reg = 0;
+    i_reg = 12'b11110xxx0000;
+    simulate(3, 8'b11001100);
+
+    // OR group - Only SMA is true
+    ac_reg = -1;
+    l_reg = 0;
+    i_reg = 12'b11110xxx0000;
+    simulate(4, 8'b11110000);
+
+    // OR group - SMA and SNL are true
+    ac_reg = -1;
+    l_reg = 1;
+    i_reg = 12'b11110xxx0000;
+    simulate(5, 8'b11111010);
+
+    // OR group - SZA and SNL are true
+    ac_reg = 0;
+    l_reg = 1;
+    i_reg = 12'b11110xxx0000;
+    simulate(6, 8'b11101110);
+
+    $display("---=== AND GROUP ===---");
+
+    // AND group - All of the predicates are true
+    ac_reg = 1;
+    l_reg = 0;
+    i_reg = 12'b11110xxx1000;
+    simulate(1, 8'b11111111);
+
+    // AND group - SZL is not true
+    ac_reg = 1;
+    l_reg = 1;
+    i_reg = 12'b11110xxx1000;
+    simulate(2, 8'b01010101);
+
+    // AND group - SNA is not true
+    ac_reg = 0;
+    l_reg = 0;
+    i_reg = 12'b11110xxx1000;
+    simulate(3, 8'b00110011);
+
+    // AND group - SPA is not true
+    ac_reg = -1;
+    l_reg = 0;
+    i_reg = 12'b11110xxx1000;
+    simulate(4, 8'b00001111);
+
+    // AND group - SPA and SZL are not true
+    ac_reg = -1;
+    l_reg = 1;
+    i_reg = 12'b11110xxx1000;
+    simulate(5, 8'b00000101);
+
+    // AND group - SNA and SZL are not true
+    ac_reg = 0;
+    l_reg = 1;
+    i_reg = 12'b11110xxx1000;
+    simulate(6, 8'b00010001);
+
+end
 
 endmodule
